c7967aadeaf98d3d1bf7cb95024c6a2fff7a73ea
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-08-04 19:03:15 +0300

    Update task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,11 +5,24 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 function createMarkup(arr) {
-  return arr.map(({ preview, original }) => `<li class="gallery__link gallery__item "><img class="gallery__image" src='${preview}' alt=""></li>`).join('');
+    return arr.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"/>
+       </a>
+     </li>`).join('');
+    
 }
 
 
 gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 
+// gallery.addEventListener('click', onClickImage)
 
-console.log(galleryItems);
+// function onClickImage(evt) {
+//     return
+// }
