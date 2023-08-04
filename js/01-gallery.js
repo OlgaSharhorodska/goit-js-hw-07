84015f439ad9559f37e979b0c93e62050b2bb3ee
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-08-04 18:21:55 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  return arr.map(({ preview }) => `<li class="gallery__image"><img src='${preview}' alt=""></li>`).join('');
+}
+
+
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+
 console.log(galleryItems);
